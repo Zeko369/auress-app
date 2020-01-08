3d515b3c3150447db7ae7fbb29ae7f2a6395f426
Fran Zekan <zekan.fran369@gmail.com>
2020-01-08 23:52:37 +0100

    Refactor connect to use async/await, add sendAnser and text

--- foobar.ts
+++ foobar.ts
@@ -1,37 +1,50 @@
 import axios from 'axios';
-import CookieManager from 'react-native-cookie-store';
+// import CookieManager from 'react-native-cookie-store';
 import {extractConfig} from './services/parsers';
 
 const URL = 'https://auress.org/s/';
 
+const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:71.0) Gecko/20100101 Firefox/71.0',
+};
+
+const post = async (body: string) => {
+  return fetch(URL, {
+    method: 'POST',
+    body,
+    credentials: 'include',
+    headers,
+  });
+};
+
 export const connect = async (roomId: number) => {
-  return CookieManager.clearAll().then(() =>
-    axios
-      .get(URL)
-      .then(data => {
-        console.log('SetCookie: ', data.headers['set-cookie']);
+  const initResponse = await axios.get(URL);
+  const setCookie = initResponse.headers['set-cookie'];
 
-        return CookieManager.get(URL);
-      })
-      .then(() => {
-        const loginData = `idSobe=${roomId}&udiUSobu=Start%0D%0A`;
+  if (setCookie === undefined) {
+    const body = initResponse.data;
+    return {status: 'Already Logged in', config: extractConfig(body)};
+  } else {
+    const loginData = `idSobe=${roomId}&udiUSobu=Start%0D%0A`;
 
-        return fetch(URL, {
-          method: 'POST',
-          body: loginData,
-          credentials: 'include',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent':
-              'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:71.0) Gecko/20100101 Firefox/71.0',
-          },
-        });
-      })
-      .then(data => {
-        return data.text();
-      })
-      .then(html => {
-        return extractConfig(html);
-      }),
-  );
+    const res = await post(loginData);
+    const html = await res.text();
+
+    return {
+      status: 'Created new login',
+      config: extractConfig(html),
+    };
+  }
+};
+
+export const sendShort = async (answer: string) => {
+  const answerData = `odgovor=${answer.toUpperCase()}&browser=App&device=Apple`;
+  return await post(answerData);
+};
+
+export const sendText = async (text: string) => {
+  const answerData = `porukaStudenta=${text}&posaljiPoruku=Send+text`;
+  return await post(answerData);
 };
