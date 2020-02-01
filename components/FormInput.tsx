10c13fc69765d743a5e7c0964bd9fec0a83783e9
Fran Zekan <zekan.fran369@gmail.com>
2020-02-01 13:38:37 +0100

    Add types for formInput, refactor styles in formInput

--- components/FormInput.tsx
+++ components/FormInput.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import {View, TextInput, TouchableOpacity, Text} from 'react-native';
+import React, {SetStateAction} from 'react';
+import {
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  GestureResponderEvent,
+  StyleSheet
+} from 'react-native';
 
-// type="numeric"
-//         label="Join room"
-//         return="go"
+import {ReturnType, Type} from '../ts/FormInput';
 
-const FormInput = ({
+interface FormInputProps {
+  text: string;
+  setText: React.Dispatch<SetStateAction<string>>;
+  onSubmit: ((event: GestureResponderEvent) => void) | undefined;
+  type?: Type;
+  label?: string;
+  returnType?: ReturnType;
+  color?: string;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
   text,
   setText,
   onSubmit,
@@ -13,43 +28,42 @@ const FormInput = ({
   label,
   returnType,
   color
-}) => {
-  return (
-    <View style={{flexDirection: 'row'}}>
-      <TextInput
-        value={text}
-        onChangeText={t => setText(t)}
-        style={{
-          flex: 3,
-          margin: 10,
-          color: color ?? 'white',
-          fontSize: 20,
-          borderRadius: 5,
-          padding: 10,
-          borderColor: 'white',
-          borderWidth: 1,
-          borderStyle: 'solid'
-        }}
-        keyboardType={type}
-        returnKeyType={returnType}
-      />
-      <TouchableOpacity
-        style={{
-          padding: 10,
-          flex: 1,
-          backgroundColor: 'purple',
-          margin: 10,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 5
-        }}
-        onPress={onSubmit}>
-        <Text style={{color: 'white', fontSize: 15, fontWeight: '800'}}>
-          {label}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+}) => (
+  <View style={{flexDirection: 'row'}}>
+    <TextInput
+      value={text}
+      onChangeText={t => setText(t)}
+      style={{...styles.textInput, color: color ?? 'white'}}
+      keyboardType={type}
+      returnKeyType={returnType}
+    />
+    <TouchableOpacity style={styles.submitButton} onPress={onSubmit}>
+      <Text style={styles.label}>{label}</Text>
+    </TouchableOpacity>
+  </View>
+);
 
 export default FormInput;
+
+const styles = StyleSheet.create({
+  textInput: {
+    flex: 3,
+    margin: 10,
+    fontSize: 20,
+    borderRadius: 5,
+    padding: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+    borderStyle: 'solid'
+  },
+  submitButton: {
+    padding: 10,
+    flex: 1,
+    backgroundColor: 'purple',
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  label: {color: 'white', fontSize: 15, fontWeight: '800'}
+});
---
+++ ts/FormInput.d.ts
@@ -0,0 +1,29 @@
+export type Type =
+  | 'default'
+  | 'email-address'
+  | 'numeric'
+  | 'phone-pad'
+  | 'visible-password'
+  | 'ascii-capable'
+  | 'numbers-and-punctuation'
+  | 'url'
+  | 'number-pad'
+  | 'name-phone-pad'
+  | 'decimal-pad'
+  | 'twitter'
+  | 'web-search';
+
+export type ReturnType =
+  | 'none'
+  | 'search'
+  | 'default'
+  | 'done'
+  | 'go'
+  | 'next'
+  | 'send'
+  | 'previous'
+  | 'google'
+  | 'join'
+  | 'route'
+  | 'yahoo'
+  | 'emergency-call';
